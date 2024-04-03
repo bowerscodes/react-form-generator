982ef75e8949ad5d5f8ca1ba7bc854dc5f18a3ee
James Bowers <jamesbowers123@gmail.com>
2024-04-03 18:54:09 +0100

    add optional Hint to FormSection and refactor as necessary

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
-import { Heading } from 'react-component-library';
+import { Heading, Hint } from 'react-component-library';
 
 import FormField from '../FormField/FormField';
 import { InputField } from '../../utils/hooks/useGetInputField';
 import { FormFieldProps } from '../FormField/FormField';
+import './FormSection.scss';
 
 
 export interface FormSectionProps {
   sectionId: string;
   sectionTitle?: string;
+  sectionHint?: string;
   fields: FormFieldProps[];
 };
-
 
 
 const FormSection = ({
   sectionId,
   sectionTitle,
+  sectionHint,
   fields
 }: FormSectionProps) => {
   
@@ -29,6 +31,7 @@ const FormSection = ({
     <section id={sectionId}>
       <fieldset>
         {sectionTitle && <legend>{<Heading id={`${sectionId}--heading`}>{sectionTitle}</Heading>}</legend>}
+        {sectionHint && <Hint id={`${sectionId}--hint`}>{sectionHint}</Hint>}
         {formFields}
       </fieldset>
     </section>
